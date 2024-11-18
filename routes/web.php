342f3d73e4cc59999b84eda76b866822e01e2400
Hadia Malick <hadia.malick@nextgeni.com>
2024-11-18 16:28:42 +0500

    Created signup and login routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\SignupController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -22,6 +24,5 @@ Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
 
-//Route::post('/posts/create', [PostController::class, 'create'])->name('posts.create');
-
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/signup', [AuthController::class, 'showSignupForm'])->name('signup');
+Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
