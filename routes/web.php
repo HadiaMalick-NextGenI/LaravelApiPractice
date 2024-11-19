bf94b07bc1291ea31a6cbde5a8233ded8fc36f52
Hadia Malick <hadia.malick@nextgeni.com>
2024-11-19 16:34:21 +0500

    Added route to access a single post

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/posts',[PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts/edit/{post}', [PostController::class, 'edit'])->name('posts.edit');
 
