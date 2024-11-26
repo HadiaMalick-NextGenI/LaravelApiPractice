9edf3e58e91819f0a9366c3a1829dc17b82250a0
Hadia Malick <hadia.malick@nextgeni.com>
2024-11-26 09:35:46 +0500

    Welcome email view

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to Our Blogging Platform!</title>
+</head>
+<body>
+    <h1>Hello, {{ $user->name }}!</h1>
+    <p>Welcome to our blogging platform! We're thrilled to have you onboard.</p>
+    <p>Start exploring and writing amazing blogs today!</p>
+    <p>Happy Blogging,</p>
+    <p>The Blog App Team</p>
+</body>
+</html>
