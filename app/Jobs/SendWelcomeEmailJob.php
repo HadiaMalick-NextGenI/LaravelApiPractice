4bd630a3641ed1eca2b9d7d07310760ae40da909
Hadia Malick <hadia.malick@nextgeni.com>
2024-11-26 09:37:38 +0500

    Welcome email job class

---
+++ app/Jobs/SendWelcomeEmailJob.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\WelcomeEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+
+class SendWelcomeEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $user;
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Log::info("Sending email to {$this->user->email}");
+        Mail::to($this->user->email)->queue(new WelcomeEmail($this->user));
+    }
+}
