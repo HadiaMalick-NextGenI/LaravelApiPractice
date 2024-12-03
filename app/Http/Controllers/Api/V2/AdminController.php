578f5d321948a032a0bdc069e825df8dde509867
Hadia Malick <hadia.malick@nextgeni.com>
2024-12-03 09:44:03 +0500

    Implemented try catch blocks

--- app/Http/Controllers/Api/V2/AdminController.php
+++ app/Http/Controllers/Api/V2/AdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Api\V2;
 
+use App\Helpers\ApiResponse;
 use App\Http\Controllers\Controller;
 use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
 use Spatie\Permission\Models\Role;
 
@@ -11,37 +13,53 @@ class AdminController extends Controller
 {
     public function index()
     {
-        $users = User::with('roles')->get();
-        return response()->json(['users' => $users]);
+        try{
+            $users = User::with('roles')->get();
+            return response()->json(['users' => $users]);
+        }catch(Exception $e){
+            return response()->json(['message' => $e->getMessage()]);
+        }
     }
 
     public function show($id)
     {
-        $user = User::with('roles')->findOrFail($id);
-        
-        return response()->json([
-            'user' => $user,
-            'roles' => $user->roles->pluck('name') 
-        ]);
+        try{
+            $user = User::with('roles')->findOrFail($id);
+            
+            return response()->json([
+                'user' => $user,
+                'roles' => $user->roles->pluck('name') 
+            ]);
+        }catch(Exception $e){
+            return ApiResponse::error('User not found',404);
+        }
     }
 
     public function assignRole(Request $request, $id)
     {
-        $user = User::findOrFail($id);
-        $role = Role::findByName($request->role);
+        try{
+            $user = User::findOrFail($id);
+            $role = Role::findByName($request->role);
 
-        if ($role) {
-            $user->syncRoles([$role->name]);
-            return response()->json(['message' => 'Role assigned successfully.']);
-        } else {
-            return response()->json(['message' => 'Role not found.'], 404);
+            if ($role) {
+                $user->syncRoles([$role->name]);
+                return response()->json(['message' => 'Role assigned successfully.']);
+            } else {
+                return response()->json(['message' => 'Role not found.'], 404);
+            }
+        }catch(Exception $e){
+            return response()->json(['message' => $e->getMessage()]);
         }
     }
 
     public function destroy($id)
     {
-        $user = User::findOrFail($id);
-        $user->delete();
-        return response()->json(['message' => 'User deleted successfully.']);
+        try{
+            $user = User::findOrFail($id);
+            $user->delete();
+            return response()->json(['message' => 'User deleted successfully.']);
+        }catch(Exception $e){
+            return ApiResponse::error('User not found',404);
+        }
     }
 }
